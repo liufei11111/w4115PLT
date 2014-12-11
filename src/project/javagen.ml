295ed65f450364db22a0fbff9fbaac9062d9793e
Fei Liu <liufei11111@hotmail.com>
2014-12-11 16:07:49 -0500

    update angelaz1.az and Matrix.java adding addByConstant

--- src/project/javagen.ml
+++ src/project/javagen.ml
@@ -1,5 +1,4 @@
 open Printf
-open Random
 open Ast
 (**
 	Previous results
@@ -40,55 +39,59 @@ let rec tuple_id (id,nums) = match nums with
 | [] -> []
 | head :: tail -> (id,head):: tuple_id (id,tail)
 (*string for expression*)
-let rec string_of_expr = function
+let rec string_of_expr_javagen = function
     Float_lit(l) -> string_of_float l
 	| Int_lit(l) -> string_of_int l
 	| String_lit(l) -> "\"" ^ l ^ "\""
   | Id(s) -> s
   | Binary_op(e1, o, e2) ->
-      string_of_expr e1 ^ " " ^
+      string_of_expr_javagen e1 ^ " " ^
       (match o with
 			| (*And | Or | Eq | Neq | Lt | Gt | Leq | Geq*)
 			Add -> "+" | Sub -> "-" | Times -> "*" | Divide -> "/" | Eq -> "=="
 			| Neq -> "!=" 			| Lt -> "<" | Leq -> "<=" | Gt -> ">" | Geq -> ">=" 
 			| And -> "&&" | Or -> "||" 
       ) ^ " " ^
-      string_of_expr e2
+      string_of_expr_javagen e2
 	| MatBinary_op(e1, o, e2) ->
       (match o with
-			MAdd -> "MatrixMathematics.add("^string_of_expr e1^" ,"^string_of_expr e2^")" 
-			| MSub -> "MatrixMathematics.subtract("^string_of_expr e1^" ,"^string_of_expr e2^")" 
-			| MTime -> "MatrixMathematics.multiply("^string_of_expr e1^" ,"^string_of_expr e2^")"  
-			| MDivide -> "MatrixMathematics.multiply("^string_of_expr e1^" ,"^"MatrixMathematics.inverse("^string_of_expr e2^"))"
-			| MITime  ->  "("^string_of_expr e1^").multiplyByConstant("^string_of_expr e2^")"
-			| MIDivide  ->  "("^string_of_expr e1^").multiplyByConstant(1.000/("^string_of_expr e2^"))"
+			MAdd -> "MatrixMathematics.add("^string_of_expr_javagen e1^" ,"^string_of_expr_javagen e2^")" 
+			| MSub -> "MatrixMathematics.subtract("^string_of_expr_javagen e1^" ,"^string_of_expr_javagen e2^")" 
+			| MTime -> "MatrixMathematics.multiply("^string_of_expr_javagen e1^" ,"^string_of_expr_javagen e2^")"  
+			| MDivide -> "MatrixMathematics.multiply("^string_of_expr_javagen e1^" ,"^"MatrixMathematics.inverse("^string_of_expr_javagen e2^"))"
+			| MITime  ->  "("^string_of_expr_javagen e1^").multiplyByConstant("^string_of_expr_javagen e2^")"
+			| MIDivide  ->  "("^string_of_expr_javagen e1^").multiplyByConstant(1.000/("^string_of_expr_javagen e2^"))"
+			| MIAdd  ->  "("^string_of_expr_javagen e1^").addByConstant("^string_of_expr_javagen e2^")"
+			| MISub  ->  "("^string_of_expr_javagen e1^").addByConstant(-1*("^string_of_expr_javagen e2^"))"
 			) 
       
-  | VarAssign(v, e) -> string_of_expr v ^ " = " ^ string_of_expr e
-	| Matrix_element (v,e1,e2)->v^".data" ^"["^ string_of_expr e1^"]["^ string_of_expr e2^"] "
+  | VarAssign(v, e) -> string_of_expr_javagen v ^ " = " ^ string_of_expr_javagen e
+	| Matrix_element (v,e1,e2)->v^".data" ^"["^ string_of_expr_javagen e1^"]["^ string_of_expr_javagen e2^"] "
+	(* functions that are built in are listed here*)
   | Call(f, el) -> (match f with
-		| "printM" -> "("^String.concat ", " (List.map string_of_expr el)^").print()"
-		|_  ->  f^"(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+		| "printM" -> "("^String.concat ", " (List.map string_of_expr_javagen el)^").print()"
+		(*| "toInt" -> "Integer.parseInt("^String.concat ", " (List.map (int_of_string  el)))^")"*)
+		|_  ->  f^"(" ^ String.concat ", " (List.map string_of_expr_javagen el) ^ ")"
 		)
   | Noexpr -> "void"
-	| Precedence_expr(e) -> "( " ^ string_of_expr e ^ " )"
+	| Precedence_expr(e) -> "( " ^ string_of_expr_javagen e ^ " )"
 	| Struct_element(struct_id, element_id) -> struct_id^".valMap" ^ ".get(\"" ^ element_id^"\")"
 	| _ -> "space"
 (*string for struct*)
-let string_of_struct_arg (struct_id,arg) = struct_id^".valMap.put(\""^arg.id ^ "\" , " ^ string_of_expr arg.value^")"
+let string_of_struct_arg (struct_id,arg) = struct_id^".valMap.put(\""^arg.id ^ "\" , " ^ string_of_expr_javagen arg.value^")"
 
 (*string of statement*)	 
 let rec string_of_stmt = function
     Block(stmts) -> "{\n  " ^ String.concat "  " (List.map string_of_stmt stmts) ^ "}\n"
-	| Expr(expr) -> string_of_expr expr ^ ";\n"
-  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n"
-  (*| If(e, s, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s*)
-  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ") " ^
+	| Expr(expr) -> string_of_expr_javagen expr ^ ";\n"
+  | Return(expr) -> "return " ^ string_of_expr_javagen expr ^ ";\n"
+  (*| If(e, s, Block([])) -> "if (" ^ string_of_expr_javagen e ^ ")\n" ^ string_of_stmt s*)
+  | If(e, s1, s2) ->  "if (" ^ string_of_expr_javagen e ^ ") " ^
       string_of_stmt s1 ^ "else " ^ string_of_stmt s2
   | For(e1, e2, e3, s) ->
-      "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
-      string_of_expr e3  ^ ") " ^ string_of_stmt s
-  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+      "for (" ^ string_of_expr_javagen e1  ^ " ; " ^ string_of_expr_javagen e2 ^ " ; " ^
+      string_of_expr_javagen e3  ^ ") " ^ string_of_stmt s
+  | While(e, s) -> "while (" ^ string_of_expr_javagen e ^ ") " ^ string_of_stmt s
 	| Vardec(vdecl) -> string_of_vdecl vdecl ^ ";\n"
 	| Matdec(mdecl) -> string_of_mdecl mdecl ^ ";\n"
 	| Structdec (struct_id, argList) -> "Structure " 
@@ -99,7 +102,7 @@ let rec string_of_stmt = function
 	^ struct_id ^ " = new Option();\n" ^
 	 String.concat ";\n " 
 	(List.map (string_of_struct_arg ) (tuple_id (struct_id,(List.rev argList)) )) ^ ";\n"
-	| Struct_element_assign (struct_id,member_id, expr) ->  struct_id^".valMap.put(\"" ^member_id^"\","^string_of_expr expr^");\n" 
+	| Struct_element_assign (struct_id,member_id, expr) ->  struct_id^".valMap.put(\"" ^member_id^"\","^string_of_expr_javagen expr^");\n" 
 (*function decoration*)	 
 let string_of_fdecl fdecl =
  "public static "^string_of_dataType fdecl.ret ^" " ^ fdecl.func_name ^ "(" 
@@ -123,7 +126,7 @@ let string_of_program_gen (stmts, funcs)=
   String.concat "\n" (List.map string_of_fdecl (List.rev funcs)) ^ "\n" 
 (*io function*)
 
-let rec writeToFile fileName progString = 
+let  writeToFile fileName progString = 
   let file = open_out ("java/" ^ fileName ^ ".java") in
     let content=fprintf file "%s"  progString in
 		"Write finished!"
